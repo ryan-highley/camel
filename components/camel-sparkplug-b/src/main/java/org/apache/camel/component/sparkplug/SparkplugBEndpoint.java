950c8a6c64d5dc10723c0f81ea3217bd39274ce0
Ryan Highley <rhighley@redhat.com>
2023-06-28 15:30:39 -0500

    Archetype-generated component code

--- components/camel-sparkplug-b/src/main/java/org/apache/camel/component/sparkplug/SparkplugBEndpoint.java
+++ components/camel-sparkplug-b/src/main/java/org/apache/camel/component/sparkplug/SparkplugBEndpoint.java
@@ -1,26 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.component.sparkplug;
+
+import java.util.concurrent.ExecutorService;
 
 import org.apache.camel.Category;
 import org.apache.camel.Consumer;
 import org.apache.camel.Processor;
 import org.apache.camel.Producer;
-import org.apache.camel.support.DefaultEndpoint;
 import org.apache.camel.spi.Metadata;
 import org.apache.camel.spi.UriEndpoint;
 import org.apache.camel.spi.UriParam;
 import org.apache.camel.spi.UriPath;
-
-import java.util.concurrent.ExecutorService;
+import org.apache.camel.support.DefaultEndpoint;
 
 /**
  * Sparkplug B component which does bla bla.
  *
  * TODO: Update one line description above what the component does, and update Category.
  */
-@UriEndpoint(firstVersion = "4.0.0-SNAPSHOT", scheme = "sparkplug", title = "Camel :: Sparkplug B", syntax="sparkplug:name",
-             category = {Category.DATABASE})
+@UriEndpoint(firstVersion = "4.0.0-SNAPSHOT", scheme = "sparkplug", title = "Camel :: Sparkplug B", syntax = "sparkplug:name",
+             category = { Category.DATABASE })
 public class SparkplugBEndpoint extends DefaultEndpoint {
-    @UriPath @Metadata(required = true)
+    @UriPath
+    @Metadata(required = true)
     private String name;
     @UriParam(defaultValue = "10")
     private int option = 10;
